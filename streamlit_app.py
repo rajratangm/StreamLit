92d24d5bce263ab4a1009eaeeca71d776a635a4a
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-10-22 03:16:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ with st.sidebar:
           'flipper_length_mm': flipper_length_mm,
           'body_mass_g': body_mass_g,
           'sex': gender}
-  data
+  input_df = pd.DataFrame(data, index=[0])
+  input_penguins = pd.concat([input_df, X_raw], axis=0)
+input_penguins
 
   

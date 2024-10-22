36069f06c2f6b7f9d48b5359d0e3351210d3215c
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-10-22 02:41:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ st.title('🎈 Machine Learning App by Rajratan')
 st.info('This is a practice app')
 with st.expander('Data'):
   st.write('**Raw Data**')
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
 
-st.write(df)
+st.write('**X**')
+
+X = df.drop('species', axis=1)
+X 
+st.write('**y**')
+y = df.species
+y

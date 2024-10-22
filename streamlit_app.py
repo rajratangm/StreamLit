1a333fc8e5bf92e5406bb65b27b63da7253b22c0
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-10-22 02:37:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-
+import pandas as pd 
 st.title('🎈 Machine Learning App by Rajratan')
 st.info('This is a practice app')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
-st.write('Hello world!')
+st.write(df)

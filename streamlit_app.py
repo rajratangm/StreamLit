29abfa021ed4d3edc3e14833fc81ccd7285ab8ca
Rajratan Gulab More <145323630+rajratangm@users.noreply.github.com>
2024-10-22 22:54:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,12 +104,9 @@
 #     new_page1()
 
 
-import pandas as pd
-import numpy as np
-import streamlit as st
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['A', 'B', 'C'])
-st.line_chart(chart_data)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.write("File uploaded successfully!")
 
 
 
